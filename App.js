927531e679d61adc021b8fc178fe2429ea31983e
John Thomson <john.thomson@fanduel.com>
2018-11-16 15:19:23 +0000

    course completion result

--- App.js
+++ App.js
@@ -2,19 +2,99 @@ import React, { Component } from "react";
 import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
-
+import Highlight from "react-native-highlight-words";
 import { fetchWeather } from "./weatherAPI";
+
+const iconNames = {
+  Default: "md-time",
+  Clear: "md-sunny",
+  Rain: "md-rainy",
+  Thunderstorm: "md-thunderstorm",
+  Clouds: "md-cloudy",
+  Haze: "md-cloudy",
+  Snow: "md-snow",
+  Drizzle: "md-umbrella"
+};
+
+const phrases = {
+  Default: {
+    title: "Fetching the fucking weather.",
+    subtitle: "Be patient, you're witnessing a miracle.",
+    highlight: "fucking",
+    color: "#636363",
+    background: "#9c9c9c"
+  },
+  Clear: {
+    title: "It's fucking beautiful.",
+    subtitle: "Rock that shit.",
+    highlight: "fucking",
+    color: "#e32500",
+    background: "#FFD017"
+  },
+  Rain: {
+    title: "It's fucking raining now.",
+    subtitle: "Just look outside.",
+    highlight: "raining",
+    color: "#004a96",
+    background: "#2f343a"
+  },
+  Thunderstorm: {
+    title: "Pikachu, use fucking thunderbolt.",
+    subtitle: "Unplug your devices.",
+    highlight: "fucking",
+    color: "#fbff46",
+    background: "#020202"
+  },
+  Clouds: {
+    title: "Fucking fifty shades of grey.",
+    subtitle: "Fucking grey clouds everywhere.",
+    highlight: "shades",
+    color: "#0044ff",
+    background: "#939393"
+  },
+  Snow: {
+    title: "Fucking ice age.",
+    subtitle: "The movie was great, the weather is not.",
+    highlight: "ice",
+    color: "#021d4c",
+    background: "#15a678"
+  },
+  Drizzle: {
+    title: "It's fucking driech.",
+    subtitle: "Och aye the noo. Freedom.",
+    highlight: "driech",
+    color: "#b3f6e4",
+    background: "#1fbb68"
+  },
+  Haze: {
+    title: "Fucking fifty shades of grey.",
+    subtitle: "Fucking grey clouds everywhere.",
+    highlight: "shades",
+    color: "#0044ff",
+    background: "#939393"
+  }
+};
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentWillMount() {
+    this.state = { temp: 0, weather: "Default" };
+  }
+
   componentDidMount() {
     this.getLocation();
-    fetchWeather(30, 32);
   }
 
   getLocation() {
     navigator.geolocation.getCurrentPosition(
-      posData => console.log(posData),
+      posData =>
+        fetchWeather(posData.coords.latitude, posData.coords.longitude).then(
+          res =>
+            this.setState({
+              temp: Math.round(res.temp),
+              weather: res.weather
+            })
+        ),
       error => alert(error),
       { timeout: 10000 }
     );
@@ -22,17 +102,27 @@ export default class App extends Component<Props> {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: phrases[this.state.weather].background }
+        ]}
+      >
         <StatusBar hidden={true} />
         <View style={styles.header}>
-          <Icon name="ios-sunny" size={80} color="white" />
-          <Text style={styles.temp}>24°</Text>
+          <Icon name={iconNames[this.state.weather]} size={80} color="white" />
+          <Text style={styles.temp}>{this.state.temp}°</Text>
         </View>
         <View style={styles.body}>
-          <Text style={styles.title}>
-            Build a <Text style={{ color: "red" }}>Fucking</Text> Weather App
+          <Highlight
+            style={styles.title}
+            highlightStyle={{ color: phrases[this.state.weather].color }}
+            searchWords={[phrases[this.state.weather].highlight]}
+            textToHighlight={phrases[this.state.weather].title}
+          />
+          <Text style={styles.subtitle}>
+            {phrases[this.state.weather].subtitle}
           </Text>
-          <Text style={styles.subtitle}>Let's Make It Rain</Text>
         </View>
       </View>
     );
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,7 +1,15 @@
 const rootUrl =
-  "api.openweathermap.org/data/2.5/weather?appid=91fdd8cf4d600dc28ab8e347bc88f1dd";
+  "https://api.openweathermap.org/data/2.5/weather?appid=91fdd8cf4d600dc28ab8e347bc88f1dd";
 
 export const fetchWeather = (lat, lon) => {
-  const url = rootUrl + "&lat=" + lat + "&lon=" + lon;
+  const url = rootUrl + "&lat=" + lat + "&lon=" + lon + "&units=metric";
   console.log(url);
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => ({
+      temp: json.main.temp,
+      weather: json.weather[0].main
+    }))
+    .catch(err => console.log(err.message));
 };
